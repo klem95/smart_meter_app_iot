1778357285806cfc7f73e5496953403b22d35ba9
klem95 <enxbox@hotmail.com>
2020-04-28 13:08:06 +0200

    minor changes

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -8,6 +8,7 @@ const adminRouter = Router()
 adminRouter.get('/avg-spending/:id',adminValidators.avgSpending, admin.avgSpending)
 adminRouter.get('/return-users', admin.returnUsers)
 adminRouter.get('/return-samples/:id',adminValidators.ReturnSamples, admin.ReturnSamples)
+// test
 
 
 export default adminRouter
